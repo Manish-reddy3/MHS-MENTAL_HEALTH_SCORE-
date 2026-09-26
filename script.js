d3a04f2477422154d02ef0c519851db13501c474
Manish-reddy3 <manishreddybandyala@gmail.com>
2026-09-26 10:39:11 +0530

    1st

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const API_URL = "https://mhs-mental-health-score-dqim.onrender.com";
+const API_URL = "http://127.0.0.1:8000";
 
 // ---- Adjust this to match your model's actual output scale ----
 const SCORE_MIN = 0;
